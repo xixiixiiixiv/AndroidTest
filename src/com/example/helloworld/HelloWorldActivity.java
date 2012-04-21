6d5a180fd82057732f72c634151e0d07d47fee0e
xixiixiiixiv <xixiixiiixiv@gmail.com>
2012-04-21 20:19:28 +0900

    test

---
+++ src/com/example/helloworld/HelloWorldActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("HELLO", "Hello Android!!!");
+        setContentView(R.layout.main);
+    }
+}
